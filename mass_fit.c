21ec67034a049cf449d13d4ee9ae507dcdbdd643
Laura <Sinkunaite>
2015-12-17 21:11:04 +0000

    Added selection cuts

--- mass_fit.c
+++ mass_fit.c
@@ -22,19 +22,35 @@ void mass_fit()
 {
 TFile *file = new TFile("/nfs/lhcb/malexander01/charm/4pi/data/full-run-I/full_2011_data.root");
 TTree *tree = (TTree*) file->Get("Dst2010ToD0TopipipipipiTuple/DecayTree;1");
+tree->SetBranchStatus("*", 0) ;
+tree->SetBranchStatus("D0_M", 1) ;
+tree->SetBranchStatus("D0_L0HadronDecision_TOS", 1);
+tree->SetBranchStatus("lab0_L0Global_TIS", 1);
+tree->SetBranchStatus("D0_Hlt1TrackAllL0Decision_TOS", 1);
+tree->SetBranchStatus("lab0_Hlt2CharmHadD02HHHHDecision_TOS", 1);
+tree->SetBranchStatus("piplus_P", 1);
+tree->SetBranchStatus("piplus_PT",1);
 
-RooRealVar D0_M("D0_M","D0_M",1780,1950);
+//RooRealVar D0_M("D0_M","D0_M",1780,1950);
+RooRealVar D0_M("D0_M","D0_M",1805,1925);
+RooRealVar D0_L0HadronDecision_TOS("D0_L0HadronDecision_TOS","D0_L0HadronDecision_TOS",0,2);
+RooRealVar lab0_L0Global_TIS("lab0_L0Global_TIS","lab0_L0Global_TIS",0,2);
+RooRealVar lab0_Hlt2CharmHadD02HHHHDecision_TOS("lab0_Hlt2CharmHadD02HHHHDecision_TOS","lab0_Hlt2CharmHadD02HHHHDecision_TOS",0,2);
+RooRealVar D0_Hlt1TrackAllL0Decision_TOS("D0_Hlt1TrackAllL0Decision_TOS","D0_Hlt1TrackAllL0Decision_TOS",0,2);
+RooRealVar piplus_P("piplus_P","piplus_P",0,20e6);
+RooRealVar piplus_PT("piplus_PT","piplus_PT",0,540e3);
 
 // --Constructors--
 // const char* name,const char* title,const RooArgSet &vars,const char* wgtVarName = 0 (for an empty ds)
 // const char* name,const char* title,TTree* intree,const RooArgSet& vars,const char* selExpr = 0,const char* wgtVarName = 0 (obtained from tree) 
 
-//RooDataSet data("data","data",D0_M); // empty dataset to check whether fit works
-RooDataSet data("data","data",tree,RooArgSet(D0_M)); // stops here***********************************************
+//RooDataSet data("data","data",tree,RooArgSet(D0_M,lab0_Hlt2CharmHadD02HHHHDecision_TOS));
+RooDataSet data("data","data",RooArgSet(D0_M,D0_L0HadronDecision_TOS,lab0_L0Global_TIS,lab0_Hlt2CharmHadD02HHHHDecision_TOS,D0_Hlt1TrackAllL0Decision_TOS,piplus_P,piplus_PT),Import(*tree),Cut("(D0_L0HadronDecision_TOS||lab0_L0Global_TIS)&&lab0_Hlt2CharmHadD02HHHHDecision_TOS&&D0_Hlt1TrackAllL0Decision_TOS&&(piplus_P>3000)&&(piplus_PT>350)"));
+//RooDataSet data = data.reduce("lab0_Hlt2CharmHadD02HHHHDecision_TOS && D0_Hlt1TrackAllL0Decision_TOS && (lab0_L0Global_TIS || D0_L0HadronDecision_TOS) && piplus_P > 3000 && piplus_PT > 350");
 
 // Gaussian signal PDF
 RooRealVar sigmean("sigmean","D^{0} mass",1865,1805,1925);
-RooRealVar sigwidth("sigwidth","D^{0} width",10.,0.5,50.);
+RooRealVar sigwidth("sigwidth","D^{0} width",2.5,0.,10.);
 RooGaussian signal("signal","Signal PDF",D0_M,sigmean,sigwidth);
 
 /*
@@ -45,7 +61,7 @@ RooPolynomial bkg("bkg","bkg",D0_M,RooArgSet(a_coeff,b_coeff));
 */
 
 // Expo bakcground
-RooRealVar expo("expo", "expo shape parameter", -0.1, -5., 0.); 
+RooRealVar expo("expo", "expo shape parameter", 0., -10., 10.); 
 RooExponential bkg("bkg", "bkg", D0_M, expo); 
 
 RooRealVar bkgfrac("bkgfrac","bkgfrac",.9,0.,1.);
@@ -59,5 +75,5 @@ RooPlot* frame = D0_M.frame();
 data.plotOn(frame);
 model.plotOn(frame);
 frame->Draw();
-c.SaveAs("D0_mass_fit.pdf");
+c.SaveAs("D0_mass_fit_all_cuts.eps");
 }
